5a7db2ccf1277e59835c82b25b6c796271b656b3
Vincent Roberson <vincent@varmail.org>
2026-03-23 15:23:57 -0700

    moved the gpu spec check program into a new folder and created a makefile for it

--- gpu-spec-check/gpu-specs-check.cu
+++ gpu-spec-check/gpu-specs-check.cu
@@ -10,12 +10,15 @@
 
 int main() {
     cudaDeviceProp prop;
-    cudaGetDeviceProperties(&prop, 0); // <-- this is the function that does everything
+    cudaGetDeviceProperties(&prop, 0);
     printf("Device: %s\n", prop.name);
     printf("Compute Capability: %d.%d\n", prop.major, prop.minor);
     printf("SMs: %d\n", prop.multiProcessorCount);
     printf("Max threads per block: %d\n", prop.maxThreadsPerBlock);
     printf("Max threads per SM: %d\n", prop.maxThreadsPerMultiProcessor);
+    printf("Max blocks per SM: %d\n", prop.maxBlocksPerMultiProcessor);
+    printf("Max resident blocks total: %d\n", prop.maxBlocksPerMultiProcessor * prop.multiProcessorCount);
+    printf("Max resident threads total: %d\n", prop.maxThreadsPerMultiProcessor * prop.multiProcessorCount);
     printf("Shared mem per SM: %zu KB\n", prop.sharedMemPerMultiprocessor / 1024);
     printf("Warp size: %d\n", prop.warpSize);
     return 0;
